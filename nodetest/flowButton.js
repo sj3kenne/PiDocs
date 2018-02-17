44ad7c413d6019746b6163cdfd1b48117f8b9966
sj3kenne <sam.kennedy@live.ca>
2018-02-17 05:41:07 +0000

    flow works, button works, now they need to work together

--- nodetest/flowButton.js
+++ nodetest/flowButton.js
@@ -1,3 +1,4 @@
+
 var Gpio = require('onoff').Gpio;
 var LED04 = new Gpio(4, 'out'),
 LED17 = new Gpio(17, 'out'),
@@ -9,8 +10,9 @@ var leds = [LED04, LED17, LED27, LED22];
 var indexCount = 0;
 dir = "up";
 
-// Flow Controls
-var flowInterval = setInterval(flowingLeds, 200);
+//Flow Controls
+//var flowInterval = setInterval(flowingLeds, 100);
+//clearInterval(flowInterval);
 
 function flowingLeds() {
   leds.forEach(function(currentValue) {
@@ -19,39 +21,36 @@ function flowingLeds() {
   if (indexCount == 0) dir = "up";
   if (indexCount >= leds.length) dir = "down";
   if (dir == "down") indexCount--;
-  leds=[indexCount].writeSync(1);
+  leds[indexCount].writeSync(1);
   if (dir == "up") indexCount++
 };
-
-/*
-function unexportOnClose() {
-  clearInterval(flowInterval);
-  leds.forEach(function(currentValue) {
-    currentValue.writeSync(0);
-    currentValue.unexport();
-  });
-};
-// For Flow Program
-process.on('SIGINT', unexportOnClose);
-*/
 
 // Push controls
 pushButton.watch(function (err, value) {
   if (err) {
     console.error('There was an error', err);
-  return;
+    return;
   }
-  flowingLeds()
+  LED04.readSync()
+    LED04.writeSync(value);
+    LED04.readSync()
+    LED04.writeSync(value);
+  /*
+  if (value == 1){
+    //clearInterval(flowInterval);
+  } else {
+    //setInterval(flowingLeds, 100);
+  }*/
+  console.log(value);
+
 });
 
-function unexportedOnClose() {
-  clearInterval(flowInterval);
+function unexportOnClose() {
+  //clearInterval(flowInterval);
   leds.forEach(function(currentValue) {
     currentValue.writeSync(0);
     currentValue.unexport();
   });
-  //LED.writeSync(0);
-  //LED.unexport();
   pushButton.unexport();
 };
 
